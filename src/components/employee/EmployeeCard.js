b0164e68a295212c338d68309606c89c2a46a93d
Kathryn Wohl <ktwohl@gmail.com>
2020-02-21 14:06:32 -0600

    added directories and js files for employees, locations, and owners. also routed nav links to their respective components

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EmployeeCard extends Component {
+    render() {
+        return (
+            <p>Employee Card!</p>
+        )
+    }
+}
+
+export default EmployeeCard;
